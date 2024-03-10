3cad980fe3d2c9f3354e6cece6da3c75b567468a
Shreyash pant <162868052+Shreyashpant1@users.noreply.github.com>
2024-03-10 21:14:04 +0530

    linear search in java
    linear search in an array in java programming

---
+++ LinearSearch.java
@@ -0,0 +1,22 @@
+class LinearSearch {
+  static int search(int arr[],int a,int b)
+  {
+    for(int i=0;i<n;i++){
+        if(arr[i]==r
+           return 1;
+    }
+    return -1;
+  }
+
+public static void main(String[] args)
+  {
+    int[] arr={3,4,1,7,5};
+    int n=arr.length(0);
+    int x=4;
+    int index=search(arr,n,x);
+    if(index==-1)
+      system.out.println("element is not present in an array");
+    else
+      system.out.println("element found at position index");
+  }
+}
