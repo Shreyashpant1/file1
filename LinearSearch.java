bf1af5dbe0f9d74f54d01a76c819019248c79ada
Shreyash pant <162868052+Shreyashpant1@users.noreply.github.com>
2024-03-10 21:27:36 +0530

    LinearSearch.java

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,22 +1,24 @@
 class LinearSearch {
-  static int search(int arr[],int a,int b)
+  static int search(int arr[],int n,int x)
   {
-    for(int i=0;i<n;i++){
-        if(arr[i]==r
-           return 1;
+    for(int i=0;i<n;i++) {
+        if(arr[i]==x)
+           return i;
     }
     return -1;
   }
 
-public static void main(String[] args)
+  public static void main(String[] args)
   {
-    int[] arr={3,4,1,7,5};
-    int n=arr.length(0);
-    int x=4;
-    int index=search(arr,n,x);
-    if(index==-1)
-      system.out.println("element is not present in an array");
-    else
-      system.out.println("element found at position index");
+      int[] arr={ 3, 4, 1, 7, 5 };
+      int n=arr.length;
+    
+      int x=4;
+    
+      int index=search(arr, n, x);
+      if(index==-1)
+        system.out.println("element is not present in an array");
+      else
+        system.out.println("element found at position index");
   }
 }
